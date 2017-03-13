6a94b83feca90550c8696a05b5491960b2545a57
Hailin <fuhailin@hbut.edu.cn>
2017-03-13 22:00:33 +0800

    no message

--- Probabilistic-Matrix-Factorization.py
+++ Probabilistic-Matrix-Factorization.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from numpy import *
+import numpy as np
 from LoadData import load_rating_data, spilt_rating_dat
 
 
 class PMF(object):
-    def __init__(self, num_feat=10, epsilon=1, _lambda=0.1, momentum=0.8, maxepoch=100, num_batches=10,
-                 batch_size=1000):
+    def __init__(self, num_feat=10, epsilon=1, _lambda=0.1, momentum=0.8, maxepoch=20, num_batches=10, batch_size=1000):
         self.num_feat = num_feat  # Number of latent features,
         self.epsilon = epsilon  # learning rate,
         self._lambda = _lambda  # L2 regularization,
@@ -17,8 +16,8 @@ class PMF(object):
         self.num_batches = num_batches  # Number of batches in each epoch (for SGD optimization),
         self.batch_size = batch_size  # Number of training samples used in each batches (for SGD optimization)
 
-        self.w_C = None  # Movie feature vectors
-        self.w_I = None  # User feature vectors
+        self.w_Item = None  # Item feature vectors
+        self.w_User = None  # User feature vectors
 
         self.err_train = []
         self.err_val = []
@@ -38,19 +37,18 @@ class PMF(object):
         pairs_va = val_vec.shape[0]  # testdata中条目数
 
         # 1-p-i, 2-m-c
-
         num_user = int(max(np.amax(train_vec[:, 0]), np.amax(val_vec[:, 0]))) + 1  # 第0列，user总数
         num_item = int(max(np.amax(train_vec[:, 1]), np.amax(val_vec[:, 1]))) + 1  # 第1列，movie总数
 
         incremental = False
-        if ((not incremental) or (self.w_C is None)):
+        if ((not incremental) or (self.w_Item is None)):
             # initialize
             self.epoch = 0
-            self.w_C = 0.1 * np.random.randn(num_item, self.num_feat)  # numpy.random.randn 电影 M x D 正态分布矩阵
-            self.w_I = 0.1 * np.random.randn(num_user, self.num_feat)  # numpy.random.randn 用户 N x D 正态分布矩阵
+            self.w_Item = 0.1 * np.random.randn(num_item, self.num_feat)  # numpy.random.randn 电影 M x D 正态分布矩阵
+            self.w_User = 0.1 * np.random.randn(num_user, self.num_feat)  # numpy.random.randn 用户 N x D 正态分布矩阵
 
-            self.w_C_inc = np.zeros((num_item, self.num_feat))  # 创建电影 M x D 0矩阵
-            self.w_I_inc = np.zeros((num_user, self.num_feat))  # 创建用户 N x D 0矩阵
+            self.w_Item_inc = np.zeros((num_item, self.num_feat))  # 创建电影 M x D 0矩阵
+            self.w_User_inc = np.zeros((num_user, self.num_feat))  # 创建用户 N x D 0矩阵
 
         while self.epoch < self.maxepoch:
             self.epoch += 1
@@ -62,62 +60,69 @@ class PMF(object):
             # Batch update
             for batch in range(self.num_batches):
                 # print "epoch %d batch %d" % (self.epoch, batch+1)
-                batch_idx = np.mod(np.arange(self.batch_size * batch, self.batch_size * (batch + 1)),
+
+                batch_idx = np.mod(np.arange(self.batch_size * batch,
+                                             self.batch_size * (batch + 1)),
                                    shuffled_order.shape[0])  # 本次迭代要使用的索引下标
+
                 batch_invID = np.array(train_vec[shuffled_order[batch_idx], 0], dtype='int32')
                 batch_comID = np.array(train_vec[shuffled_order[batch_idx], 1], dtype='int32')
 
                 # Compute Objective Function
-                pred_out = np.sum(np.multiply(self.w_I[batch_invID, :], self.w_C[batch_comID, :]),
+                pred_out = np.sum(np.multiply(self.w_User[batch_invID, :],
+                                              self.w_Item[batch_comID, :]),
                                   axis=1)  # mean_inv subtracted
+
                 rawErr = pred_out - train_vec[shuffled_order[batch_idx], 2] + self.mean_inv
 
                 # Compute gradients
-                Ix_C = 2 * np.multiply(rawErr[:, np.newaxis], self.w_I[batch_invID, :]) + self._lambda * self.w_C[
-                                                                                                         batch_comID, :]
-                Ix_I = 2 * np.multiply(rawErr[:, np.newaxis], self.w_C[batch_comID, :]) + self._lambda * self.w_I[
-                                                                                                         batch_invID, :]
+                Ix_C = 2 * np.multiply(rawErr[:, np.newaxis], self.w_User[batch_invID, :]) \
+                       + self._lambda * self.w_Item[batch_comID, :]
+                Ix_I = 2 * np.multiply(rawErr[:, np.newaxis], self.w_Item[batch_comID, :]) \
+                       + self._lambda * self.w_User[batch_invID, :]
 
-                dw_C = np.zeros((num_item, self.num_feat))
-                dw_I = np.zeros((num_user, self.num_feat))
+                dw_Item = np.zeros((num_item, self.num_feat))
+                dw_User = np.zeros((num_user, self.num_feat))
 
                 # loop to aggreate the gradients of the same element
                 for i in range(self.batch_size):
-                    dw_C[batch_comID[i], :] += Ix_C[i, :]
-                    dw_I[batch_invID[i], :] += Ix_I[i, :]
+                    dw_Item[batch_comID[i], :] += Ix_C[i, :]
+                    dw_User[batch_invID[i], :] += Ix_I[i, :]
 
                 # Update with momentum
-                self.w_C_inc = self.momentum * self.w_C_inc + self.epsilon * dw_C / self.batch_size
-                self.w_I_inc = self.momentum * self.w_I_inc + self.epsilon * dw_I / self.batch_size
-                self.w_C = self.w_C - self.w_C_inc
-                self.w_I = self.w_I - self.w_I_inc
+                self.w_Item_inc = self.momentum * self.w_Item_inc + self.epsilon * dw_Item / self.batch_size
+                self.w_User_inc = self.momentum * self.w_User_inc + self.epsilon * dw_User / self.batch_size
+
+                self.w_Item = self.w_Item - self.w_Item_inc
+                self.w_User = self.w_User - self.w_User_inc
 
                 # Compute Objective Function after
                 if batch == self.num_batches - 1:
-                    pred_out = np.sum(np.multiply(self.w_I[np.array(train_vec[:, 0], dtype='int32'), :],
-                                                  self.w_C[np.array(train_vec[:, 1], dtype='int32'), :]),
+                    pred_out = np.sum(np.multiply(self.w_User[np.array(train_vec[:, 0], dtype='int32'), :],
+                                                  self.w_Item[np.array(train_vec[:, 1], dtype='int32'), :]),
                                       axis=1)  # mean_inv subtracted
                     rawErr = pred_out - train_vec[:, 2] + self.mean_inv
-                    obj = LA.norm(rawErr) ** 2 + 0.5 * self._lambda * (LA.norm(self.w_I) ** 2 + LA.norm(self.w_C) ** 2)
+                    obj = LA.norm(rawErr) ** 2 \
+                          + 0.5 * self._lambda * (LA.norm(self.w_User) ** 2 + LA.norm(self.w_Item) ** 2)
+
                     self.err_train.append(np.sqrt(obj / pairs_tr))
 
                 # Compute validation error
                 if batch == self.num_batches - 1:
-                    pred_out = np.sum(np.multiply(self.w_I[np.array(val_vec[:, 0], dtype='int32'), :],
-                                                  self.w_C[np.array(val_vec[:, 1], dtype='int32'), :]),
+                    pred_out = np.sum(np.multiply(self.w_User[np.array(val_vec[:, 0], dtype='int32'), :],
+                                                  self.w_Item[np.array(val_vec[:, 1], dtype='int32'), :]),
                                       axis=1)  # mean_inv subtracted
                     rawErr = pred_out - val_vec[:, 2] + self.mean_inv
                     self.err_val.append(LA.norm(rawErr) / np.sqrt(pairs_va))
 
                     # Print info
-                if batch == self.num_batches - 1:
-                    print('Training RMSE: %f, Test RMSE %f' % (self.err_train[-1], self.err_val[-1]))
-                    self.train_rmse.append(self.err_train[-1])
-                    self.test_rmse.append(self.err_val[-1])
-                    # ****************Predict rating of all movies for the given user. ***************#
+                    if batch == self.num_batches - 1:
+                        print('Training RMSE: %f, Test RMSE %f' % (self.err_train[-1], self.err_val[-1]))
+                        self.train_rmse.append(self.err_train[-1])
+                        self.test_rmse.append(self.err_val[-1])
 
     def predict(self, invID):
-        return np.dot(self.w_C, self.w_I[int(invID), :]) + self.mean_inv  # numpy.dot 点乘
+        return np.dot(self.w_Item, self.w_User[int(invID), :]) + self.mean_inv  # numpy.dot 点乘
 
     # ****************Set parameters by providing a parameter dictionary.  ***********#
     def set_params(self, parameters):
@@ -130,7 +135,7 @@ class PMF(object):
             self.num_batches = parameters.get("num_batches", 10)
             self.batch_size = parameters.get("batch_size", 1000)
 
-    def topK(self, model, test_vec, k=10):  # model TrainDataSet, test_vec
+    def topK(self, test_vec, k=10):
         inv_lst = np.unique(test_vec[:, 0])
         pred = {}
         for inv in inv_lst:
@@ -156,6 +161,8 @@ if __name__ == "__main__":
     file_path = "data/ml-100k/u.data"
     pmf = PMF()
     ratings = load_rating_data(file_path)
+    pmf.set_params({"num_feat": 10, "epsilon": 1, "_lambda": 0.1, "momentum": 0.8, "maxepoch": 100, "num_batches": 100,
+                  "batch_size": 1000})
     print(len(np.unique(ratings[:, 0])), len(np.unique(ratings[:, 1])), pmf.num_feat)
     train, test = spilt_rating_dat(ratings)
     pmf.fit(train, test)
@@ -169,3 +176,4 @@ if __name__ == "__main__":
     plt.legend()
     plt.grid()
     plt.show()
+    print("precision_acc,recall_acc:" + str(pmf.topK(test)))
