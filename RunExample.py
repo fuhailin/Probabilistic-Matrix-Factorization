63887594eff8514b23937b395de9b17733f0d146
Hailin <fuhailin@hbut.edu.cn>
2017-03-23 01:06:16 +0800

    Use a more efficient data spilt method

--- RunExample.py
+++ RunExample.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from LoadData import load_rating_data, spilt_rating_dat
+from sklearn.model_selection import train_test_split
 from ProbabilisticMatrixFactorization import PMF
 
 if __name__ == "__main__":
@@ -10,7 +11,7 @@ if __name__ == "__main__":
                     "batch_size": 1000})
     ratings = load_rating_data(file_path)
     print(len(np.unique(ratings[:, 0])), len(np.unique(ratings[:, 1])), pmf.num_feat)
-    train, test = spilt_rating_dat(ratings)
+    train, test = train_test_split(ratings, test_size=0.2)  # spilt_rating_dat(ratings)
     pmf.fit(train, test)
 
     # Check performance by plotting train and test errors
